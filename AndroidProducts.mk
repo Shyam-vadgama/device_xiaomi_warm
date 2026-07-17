ec9912d73b0a62c8247826801e489afb9e186912
Shyam Vadgama <163844065+Shyam-vadgama@users.noreply.github.com>
2026-07-17 16:28:49 +0530

    Replace pixel_warm.mk with lineage_warm.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,9 +4,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/pixel_warm.mk
-
-COMMON_LUNCH_CHOICES := \
-    pixel_warm-ap4a-userdebug \
-    pixel_warm-ap4a-user \
-    pixel_warm-ap4a-eng
+    $(LOCAL_DIR)/lineage_warm.mk
