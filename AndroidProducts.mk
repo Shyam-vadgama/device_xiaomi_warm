667b8ee693809ed4afcaf85a30591ac5d199e5ce
Shyam Vadgama <163844065+Shyam-vadgama@users.noreply.github.com>
2026-07-19 16:11:18 +0530

    Add common lunch choices for pixel_warm

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,3 +5,10 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/pixel_warm.mk
+
+COMMON_LUNCH_CHOICES := \
+    pixel_warm-bp4a-user \
+    pixel_warm-bp4a-userdebug \
+    pixel_warm-bp4a-eng  \
+    pixel_warm-ap4a-userdebug \
+    pixel_warm-userdebug
